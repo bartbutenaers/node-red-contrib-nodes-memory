9cc17978fa154d1fe6c9340d15769670c8c70f69
bartbutenaers <bart.butenaers@gmail.com>
2019-03-25 23:51:27 +0100

    Basic fixes

--- memory.js
+++ memory.js
@@ -16,36 +16,35 @@
  module.exports = function(RED) {
     var settings = RED.settings;
     var sizeof = require('object-sizeof');
+    var wildstring = require('wildstring');
 
     function MemoryAnalyserNode(config) {
         RED.nodes.createNode(this, config);
         this.typeFilter = config.typefilter || "";
         this.nameFilter = config.namefilter || "";
-        this.tabfilter = config.tabfilter || "";
         this.idfilter = config.idfilter || "";
+        this.tabidfilter = config.tabidfilter || "";
+        this.topCount = config.topCount || 0;
+        this.sort = config.sort;
         this.activefilter = config.activefilter;
         this.analysecontext = config.analysecontext;
         this.separatemsg = config.separatemsg;
-        this.top5 = config.top5;
         this.sum = config.sum;
         this.nodesToAnalyse = [];
         
         var node = this;
-                
+        
+        wildstring.caseSensitive = false; 
+               
         RED.nodes.eachNode(function(nodeToAnalyse) {
-            if (node.typeFilter.length == 0 || node.typeFilter === nodeToAnalyse.type) {
+            if (node.typeFilter.length == 0 || wildstring.match(node.typeFilter, nodeToAnalyse.type)) {
                 // We only want to analyse nodes that are used in the flow (not tabsheets, ui groups, ...)
                 // Seems that the only thing those nodes have in common, is that they have a 'wires' field...
                 if (nodeToAnalyse.wires) {
-                    if (node.nameFilter.length == 0 || node.nameFilter === nodeToAnalyse.name) {
-                        if (node.idfilter.length == 0 || node.idfilter === nodeToAnalyse.id) {
-                            // TODO tab filter werkt niet want getNode geeft enkel de nodes van de huidig zichtbare tab ...
-                            if (node.tabfilter.length == 0 || node.tabfilter === nodeToAnalyse.z) {
-                                // getNode will return the node when it is located into an active tabsheet
-                                // TODO dit werkt niet: soms zitten er geen nodes onder een tabsheet ...
-                                //if (!node.activefilter || RED.nodes.getNode(nodeToAnalyse.id)) {
-                                    node.nodesToAnalyse.push( {node:nodeToAnalyse} );
-                                //}
+                    if (node.nameFilter.length == 0 || wildstring.match(node.nameFilter, nodeToAnalyse.name)) {
+                        if (node.idfilter.length == 0 || wildstring.match(node.idfilter, nodeToAnalyse.id)) {
+                            if (node.tabidfilter.length == 0 || wildstring.match(node.tabidfilter, nodeToAnalyse.z)) {
+                                node.nodesToAnalyse.push( {node:nodeToAnalyse} );
                             }
                         }
                     }
@@ -72,19 +71,17 @@
                 analysis.name = nodeToAnalyse.node.name;
                 
                 // Calculate the new size of the node
-                analysis.node_size = sizeof(nodeToAnalyse.node);
+                analysis.size = sizeof(nodeToAnalyse.node);
                 
                 // Calculate the node size difference compared to the previous size (plus = increased / minus = decreased).
                 // For the first analysis, we will pass a zero difference
                 // TODO klopt dit wel ??????????
-                analysis.node_difference = analysis.node_size - (nodeToAnalyse.nodesize || analysis.node_size);
+                analysis.difference = analysis.size - (nodeToAnalyse.nodesize || analysis.size);
                 
                 if (node.analysecontext) {
                     // We can only get the context of other nodes by pretending we are that node.
                     // This can easily be done by using the same node id and z in this node.
                     // Since we cannot access nodeToAnalyse.node.context() here, we are going to access it via THIS node.
-                    // WATCH OUT: this is bad practice since this is straight against data encapsulation patterns.
-                    // So don't use this mechanism elsewhere to pass data between nodes ...
                     this.id = analysis.id;
                     this.z = analysis.z;
                     
@@ -99,12 +96,12 @@
                     // For the first analysis, we will pass a zero difference
                     analysis.context_difference = analysis.context_size - (nodeToAnalyse.nodesize || analysis.context_size);
                     
-                    analysis.total_size = analysis.node_size + analysis.context_size;
-                    analysis.total_difference = analysis.node_difference + analysis.context_difference;
+                    analysis.total_size = analysis.size + analysis.context_size;
+                    analysis.total_difference = analysis.difference + analysis.context_difference;
                 }
 
                 // Remember the size for the next difference calculation
-                nodeToAnalyse.nodesize = analysis.node_size;
+                nodeToAnalyse.nodesize = analysis.size;
                 
                 result.push(analysis);
             }
@@ -117,27 +114,49 @@
                 var sum = 0;
                 
                 for (var j = 0; j < result.length; j++) {
-                    sum += result[j].node_size;
+                    sum += result[j].size;
                 }
                 
-                result.push( {id: 'total_sum', node_size: sum} );
+                result.push( {id: 'total_sum', size: sum} );
             }
             
-            if (node.top5 === true) {
-                // Sort the memory sizes from high to low
-                result.sort(function(a, b) {
-                    return parseFloat(b.node_size) - parseFloat(a.node_size);
-                });
-                
-                // Keep only the 5 highest numbers
-                result.length = 5;
+            // Sort the list by memory sizes (numerically) descending
+            result.sort(function(a, b) {
+                return parseFloat(b.size) - parseFloat(a.size);
+            });
+            
+            if (node.topCount > 0) {
+                // Keep only the N highest numbers
+                result.length = Math.min(node.topCount, result.length);
+            }
+            
+            switch (node.sort) {
+                case "size":
+                    // Do nothing since the array is already ordered by size (see above)
+                    break;
+                case "name":
+                    // Sort the array by node name
+                    result.sort(function(a, b) {
+                        if(a.name < b.name) { return -1; }
+                        if(a.name > b.name) { return 1; }
+                        return 0;
+                    });
+                    break;
+                case "type":
+                    // Sort the array by node type
+                    result.sort(function(a, b) {
+                        if(a.type < b.type) { return -1; }
+                        if(a.type > b.type) { return 1; }
+                        return 0;
+                    });
+                    break;
             }
             
             if (node.separatemsg) {
                 for (var i = 0; i < result.length; i++) {
                     analysis = result[i];
                     msg.topic = analysis.id;
-                    msg.payload = analysis.node_size;
+                    msg.payload = analysis.size;
                     node.send(msg);
                 }
             }
