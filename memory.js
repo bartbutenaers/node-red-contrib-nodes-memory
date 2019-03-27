aa42ff9b89a242065a41cda96c6bad123e4950d6
bartbutenaers <bart.butenaers@gmail.com>
2019-03-27 21:07:19 +0100

    Calculations refactored

--- memory.js
+++ memory.js
@@ -20,31 +20,32 @@
 
     function MemoryAnalyserNode(config) {
         RED.nodes.createNode(this, config);
-        this.typeFilter = config.typefilter || "";
-        this.nameFilter = config.namefilter || "";
-        this.idfilter = config.idfilter || "";
-        this.tabidfilter = config.tabidfilter || "";
-        this.topCount = config.topCount || 0;
-        this.sort = config.sort;
-        this.activefilter = config.activefilter;
-        this.analysecontext = config.analysecontext;
-        this.separatemsg = config.separatemsg;
-        this.sum = config.sum;
-        this.nodesToAnalyse = [];
+        this.typeFilter           = config.typefilter || "";
+        this.nameFilter           = config.namefilter || "";
+        this.idfilter             = config.idfilter || "";
+        this.tabidfilter          = config.tabidfilter || "";
+        this.excludeid            = config.excludeid || "";
+        this.topCount             = config.topCount || 0;
+        this.sort                 = config.sort;
+        this.separatemsg          = config.separatemsg;
+        this.sum                  = config.sum;
+        this.nodesToAnalyse       = [];
         
         var node = this;
         
         wildstring.caseSensitive = false; 
                
         RED.nodes.eachNode(function(nodeToAnalyse) {
-            if (node.typeFilter.length == 0 || wildstring.match(node.typeFilter, nodeToAnalyse.type)) {
+            if (node.typeFilter.length === 0 || wildstring.match(node.typeFilter, nodeToAnalyse.type)) {
                 // We only want to analyse nodes that are used in the flow (not tabsheets, ui groups, ...)
                 // Seems that the only thing those nodes have in common, is that they have a 'wires' field...
                 if (nodeToAnalyse.wires) {
-                    if (node.nameFilter.length == 0 || wildstring.match(node.nameFilter, nodeToAnalyse.name)) {
-                        if (node.idfilter.length == 0 || wildstring.match(node.idfilter, nodeToAnalyse.id)) {
+                    if (node.nameFilter.length === 0 || wildstring.match(node.nameFilter, nodeToAnalyse.name)) {
+                        if (node.idfilter.length === 0 || wildstring.match(node.idfilter, nodeToAnalyse.id)) {
                             if (node.tabidfilter.length == 0 || wildstring.match(node.tabidfilter, nodeToAnalyse.z)) {
-                                node.nodesToAnalyse.push( {node:nodeToAnalyse} );
+                                if (node.excludeid.length == 0 || !wildstring.match(node.excludeid, nodeToAnalyse.id)) {
+                                    node.nodesToAnalyse.push( {node:nodeToAnalyse} );
+                                }
                             }
                         }
                     }
@@ -65,43 +66,44 @@
                 var nodeToAnalyse = node.nodesToAnalyse[i];
                 
                 var analysis = {};
-                analysis.id = nodeToAnalyse.node.id;
-                analysis.z = nodeToAnalyse.node.z
+                analysis.id   = nodeToAnalyse.node.id;
+                analysis.tab  = nodeToAnalyse.node.z
                 analysis.type = nodeToAnalyse.node.type;
                 analysis.name = nodeToAnalyse.node.name;
                 
                 // Calculate the new size of the node
-                analysis.size = sizeof(nodeToAnalyse.node);
+                analysis.node_size = sizeof(nodeToAnalyse.node) /*- sizeof(nodeToAnalyse.node.wires)*/;
                 
                 // Calculate the node size difference compared to the previous size (plus = increased / minus = decreased).
                 // For the first analysis, we will pass a zero difference
-                // TODO klopt dit wel ??????????
-                analysis.difference = analysis.size - (nodeToAnalyse.nodesize || analysis.size);
+                analysis.node_difference = analysis.node_size - (nodeToAnalyse.previousNodeSize || analysis.node_size);
+                                
+                // We can only get the context of other nodes by pretending we are that node.
+                // This can easily be done by using the same node id and z in this node.
+                // Since we cannot access nodeToAnalyse.node.context() here, we are going to access it via THIS node.
+                this.id = analysis.id;
+                this.z = analysis.z;
                 
-                if (node.analysecontext) {
-                    // We can only get the context of other nodes by pretending we are that node.
-                    // This can easily be done by using the same node id and z in this node.
-                    // Since we cannot access nodeToAnalyse.node.context() here, we are going to access it via THIS node.
-                    this.id = analysis.id;
-                    this.z = analysis.z;
-                    
-                    // The size of the node context is the sum of the sizes of all variables on the node context memory
-                    analysis.context_size = 0;
-                    for (var j = 0; j < node.context().keys(); j++) {
-                        var key = nodeToAnalyse.node.context().keys()[j];
-                        analysis.context_size += sizeof(nodeToAnalyse.node.context().get(key) || {});
-                    }
-                    
-                    // Calculate the context size difference compared to the previous size (plus = increased / minus = decreased).
-                    // For the first analysis, we will pass a zero difference
-                    analysis.context_difference = analysis.context_size - (nodeToAnalyse.nodesize || analysis.context_size);
-                    
-                    analysis.total_size = analysis.size + analysis.context_size;
-                    analysis.total_difference = analysis.difference + analysis.context_difference;
+                // Calculate the new size of the node context, as the sum of the sizes of all variables on the node context memory
+                analysis.context_size = 0;
+                for (var j = 0; j < node.context().keys(); j++) {
+                    var key = nodeToAnalyse.node.context().keys()[j];
+                    analysis.context_size += sizeof(nodeToAnalyse.node.context().get(key) || {});
                 }
+                
+                // Calculate the context size difference compared to the previous size (plus = increased / minus = decreased).
+                // For the first analysis, we will pass a zero difference
+                analysis.context_difference = analysis.context_size - (nodeToAnalyse.previousContextSize || analysis.context_size);
 
-                // Remember the size for the next difference calculation
-                nodeToAnalyse.nodesize = analysis.size;
+                // Calculate the total size, which contains both the node size and the context size
+                analysis.total_size = analysis.node_size + analysis.context_size;
+                
+                // Calculate the total size difference compared to the previous size (plus = increased / minus = decreased).
+                analysis.total_difference = analysis.node_difference + analysis.context_difference;
+
+                // Remember the current sizes for the next difference calculations
+                nodeToAnalyse.previousNodeSize = analysis.node_size;
+                nodeToAnalyse.previousContextSize = analysis.context_size;
                 
                 result.push(analysis);
             }
@@ -110,11 +112,19 @@
             this.id = originalId;
             this.z = originalZ;
             
+            if (node.analyseflowcontext) {
+                // TODO node.nodesToAnalyse.push( {node: 'flow'} );
+            }
+            
+            if (node.analyseglobalcontext) {
+                // TODO node.nodesToAnalyse.push( {node: 'global'} );
+            }
+            
             if (node.sum === true) {
                 var sum = 0;
                 
                 for (var j = 0; j < result.length; j++) {
-                    sum += result[j].size;
+                    sum += result[j].node_size;
                 }
                 
                 result.push( {id: 'total_sum', size: sum} );
@@ -122,7 +132,7 @@
             
             // Sort the list by memory sizes (numerically) descending
             result.sort(function(a, b) {
-                return parseFloat(b.size) - parseFloat(a.size);
+                return parseFloat(b.node_size) - parseFloat(a.node_size);
             });
             
             if (node.topCount > 0) {
@@ -156,7 +166,7 @@
                 for (var i = 0; i < result.length; i++) {
                     analysis = result[i];
                     msg.topic = analysis.id;
-                    msg.payload = analysis.size;
+                    msg.payload = analysis.node_size;
                     node.send(msg);
                 }
             }
